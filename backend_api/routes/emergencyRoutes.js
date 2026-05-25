66ca42ca010308d19ad166a9dad527e547a02a07
24043433-Afiqah <24043433@myrp.edu.sg>
2026-05-25 15:33:27 +0800

    update to routes folder

---
+++ backend_api/routes/emergencyRoutes.js
@@ -0,0 +1,37 @@
+const express = require("express");
+const router = express.Router();
+const db = require("../config/db");
+
+// ================= TRIGGER EMERGENCY =================
+router.post("/trigger", (req, res) => {
+    const { senior_id } = req.body;
+
+    const sql = `
+        INSERT INTO Emergency_Event (senior_id, event_type, event_status, escalation_level)
+        VALUES (?, 'SOS', 'Open', 'Level 1')
+    `;
+
+    db.query(sql, [senior_id], (err, result) => {
+        if (err) return res.send(err);
+
+        res.send("Emergency triggered");
+    });
+});
+
+
+// ================= GET EMERGENCY HISTORY =================
+router.get("/:senior_id", (req, res) => {
+
+    const sql = `
+        SELECT * FROM Emergency_Event
+        WHERE senior_id = ?
+        ORDER BY created_at DESC
+    `;
+
+    db.query(sql, [req.params.senior_id], (err, result) => {
+        if (err) return res.send(err);
+        res.json(result);
+    });
+});
+
+module.exports = router;
