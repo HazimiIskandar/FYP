095846bd8776f4bc6b002750a9227c2389573602
24043433-Afiqah <24043433@myrp.edu.sg>
2026-05-26 17:38:01 +0800

    update servicenow.js

--- backend_api/services/servicenow.js
+++ backend_api/services/servicenow.js
@@ -1,3 +1,7 @@
+const axios = require("axios");
+
+
+// WORKFLOW
 const triggerServiceNowWorkflow = (
     senior_id,
     event_type
@@ -9,10 +13,60 @@ const triggerServiceNowWorkflow = (
         Event: ${event_type}
     `);
 
-    // future:
-    // actual API call to ServiceNow
 };
 
+
+// REAL INCIDENT API 
+const createIncident = async (
+    short_description,
+    description
+) => {
+
+    // CHANGE THESE LATER
+    const SERVICENOW_URL =
+        "https://YOUR-INSTANCE.service-now.com/api/now/table/incident";
+
+    const USERNAME = "admin";
+    const PASSWORD = "yourpassword";
+
+    try {
+
+        const response = await axios.post(
+            SERVICENOW_URL,
+            {
+                short_description,
+                description,
+                urgency: "1",
+                impact: "1"
+            },
+            {
+                auth: {
+                    username: USERNAME,
+                    password: PASSWORD
+                },
+                headers: {
+                    "Content-Type": "application/json",
+                    "Accept": "application/json"
+                }
+            }
+        );
+
+        console.log("ServiceNow Incident Created");
+
+        return response.data;
+
+    } catch (err) {
+
+        console.log(
+            "ServiceNow Error:",
+            err.response?.data || err.message
+        );
+    }
+};
+
+
+// EXPORT BOTH
 module.exports = {
-    triggerServiceNowWorkflow
+    triggerServiceNowWorkflow,
+    createIncident
 };
