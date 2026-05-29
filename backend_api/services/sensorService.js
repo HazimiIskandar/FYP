90f64482273cefc945ead9ebab2669f96219e745
24043433-Afiqah <24043433@myrp.edu.sg>
2026-05-29 19:13:37 +0800

    update sensorService

---
+++ backend_api/services/sensorService.js
@@ -0,0 +1,14 @@
+const processSensorAlert = (
+    senior_id,
+    sensor_type
+) => {
+
+    console.log(
+        `Sensor alert from Senior ${senior_id}: ${sensor_type}`
+    );
+
+};
+
+module.exports = {
+    processSensorAlert
+};
