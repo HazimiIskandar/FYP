7e438a88f51f81de0abc8e241a8de54a9dffe888
24043433-Afiqah <24043433@myrp.edu.sg>
2026-07-11 19:48:39 +0800

    attempt tele bot v1

---
+++ backend_api/telegramRecipients.js
@@ -0,0 +1,5 @@
+// map of routing bucket + array of chat ids
+module.exports = {
+  caregiver_nok_aic: ["", ""],  // caregiver + NOK + AIC chats
+  caregiver_aic: ["", ""],      // caregiver + AIC chats
+};
